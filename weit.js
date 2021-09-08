5b9a3c1f221a7abbdfd0839553f452b796f80ee3
sirben181 <bensonkamauben@gmail.com>
2021-09-08 12:01:00 +0300

    adding the grams conversion

--- weit.js
+++ weit.js
@@ -10,13 +10,18 @@ resultTypeValue=resultType.value
 input.addEventListener('keyup',weightConverter)
 inputType.addEventListener('change',weightConverter)
 resultType.addEventListener('change',weightConverter)
+
 function weightConverter(){
     inputTypeValue=inputType.value
     resultTypeValue=resultType.value
     if( inputTypeValue==='kilogram' && resultTypeValue==='kilogram'){
         result.value=input.value
         console.log(result.value)
+    } else if(inputTypeValue==="kilogram" && resultTypeValue==="gram")
+    {
+        result.value=Number(input.value)*1000
     }
+    
   
 }
 
